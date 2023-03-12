70faa133ae2c2c34f443fda2ac7d1e8b41b979f9
Scott <saseaa37@gmail.com>
2023-03-11 18:30:24 -0700

    Added Header to Routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+#_*_ coding: utf8 _*_
+"""FSDI-111 Assignment 2"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,7 +13,7 @@ def about_me():
     me = {
         "first_name": "Scott",
         "last_name": "Stevens",
-        "hobbies": "Flying drones",
+        "hobbies": "Flying Drones",
         "active": True    
     }
 
